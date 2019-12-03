9b0c231590f7f506423b4c97bb4a685d8bbee7da
Juan Maciel <jmaciel172013@gmail.com>
2019-12-03 12:16:24 -0300

    Revert "Basic structure"
    This reverts commit 4604e9c67df594e0032d377ab2807f01d5a87d89.

--- src/config/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.Promise = global.Promise
-module.exports = mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-
-mongoose.Error.messages.general.required = "O atributo {PATH} é obrigatório."
--- src/loader.js
+++
@@ -1,4 +0,0 @@
-require('dotenv').config()
-const server = require('./config/server')
-require('./config/database')
-require('./config/routes')(server)
